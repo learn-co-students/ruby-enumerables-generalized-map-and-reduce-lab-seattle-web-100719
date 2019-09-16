0b3f560c835f5f444642e82fdf36d605a6817ca1
clever-minifier-2555 <alisabaj@gmail.com>
2019-09-16 02:53:52 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+def map(source_array)
+  counter = 0
+  new_array = []
+
+  while source_array[counter] do
+    new_array.push(yield(source_array[counter]))
+    counter +=1
+  end
+  new_array
+end
+
+
+
+def reduce(source_array, starting_value = 0)
+  counter = 0
+  result = starting_value
+
+  while counter < source_array.length do
+    result = yield(source_array[counter], result)
+    counter += 1
+  end
+
+  if result
+    return result
+  else
+    return false
+  end
+end
