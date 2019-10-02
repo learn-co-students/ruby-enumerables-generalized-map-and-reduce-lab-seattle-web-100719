ff7e728d9d31d0df33305898c1838a8a8d97e4ed
Jennifer Williams <jenken131@gmail.com>
2019-10-02 20:30:32 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
 # Your Code Here
+# def map(array)
+#   new = []
+#   i = 0;
+#   while i < array.length
+#     new.push(yield(array[i]))
+#     i += 1
+#   end
+#   new
+# end
+
+def map(array)
+  new = []
+  array.length.times do |index|
+    new.push(yield(array[index]))
+  end
+  new
+end
+
+def reduce(array, starting_point = nil)
+  if starting_point
+    sum = starting_point
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1
+  end
+  sum
+end
